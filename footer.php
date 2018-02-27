37112b619f7e98e8e691125961946b01ba1f04ee
Aleksandar Hristovski <xrristo@gmail.com>
2018-02-27 14:20:52 +0100

    bootstrap01

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 		<p>This is my footer</p>
 		<?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
 	</footer>
+	</div> <!-- .container -->
 	<?php wp_footer(); ?>
 	</body>
 </html>
--- functions.php
+++ functions.php
@@ -1,11 +1,29 @@
 <?php 
-	
+	/*
+		=================================
+			include scripts
+		=================================
+	*/	
 function awesome_script_enqueue(){
+    //css bootstrap
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', [], '3.3.7', 'all');
+    //js jquery
+    wp_enqueue_script('jquery', [], '1.0.0', true);
+    //js bootstrap
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', [], '3.3.7', true);
+    //css
     wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/awesome.css', [], '1.0.0', 'all'); 
+    //js
     wp_enqueue_script('customjs', get_template_directory_uri() . 'js/awesomejs', [], '1.0.0', true);
 }
 
 add_action( 'wp_enqueue_scripts', 'awesome_script_enqueue');
+
+	/*
+		=================================
+			activate menus
+		=================================
+	*/	
 
 function awesome_theme_setup(){
 	add_theme_support( 'menus' );
@@ -14,6 +32,11 @@ function awesome_theme_setup(){
 	register_nav_menu('secondary', 'Footer Navigation');
 }
 
+	/*
+		=================================
+			Theme support
+		=================================
+	*/	
 add_action('init', 'awesome_theme_setup');
 
 add_theme_support('custom-background');
