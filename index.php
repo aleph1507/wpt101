c2df0518216f1e7f9f9b561a8f7538340e355d4a
Aleksandar Hristovski <xrristo@gmail.com>
2018-02-28 18:06:31 +0100

    carousel_tiles01

--- index.php
+++ index.php
@@ -1,17 +1,50 @@
 <?php get_header(); ?>
 
+	
+
 	<div class="row">
 		<div class="col-xs-12 col-sm-8">
-			<?php 
-				if(have_posts()):
-					while(have_posts()): 
-						the_post(); ?>
+			<div class="row text-center">
+				
+				<?php 
+					if(have_posts()):
+						$i = 0;
+						while(have_posts()): 
+							the_post(); ?>
 
-						<?php get_template_part('content', get_post_format()); ?>
-						
-					<?php endwhile;
-				endif;
-			?>
+							<?php 
+								global $column;
+								if($i==0): $column=12; 
+									elseif($i > 0 && $i <=2): $column = 6; $class = ' second-row-padding';
+									elseif($i > 2): $column = 4; $class = ' third-row-padding';
+							     endif; 
+							?>
+							
+							<div class="col-xs-<?= $column . $class; ?>">
+								<?php 
+									if(has_post_thumbnail()):	
+										$urlImg = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID()));
+										// $urlImg = get_the_post_thumbnail_url();
+									endif;
+								?>
+								<div class="blog-element" style="background-image: url(<?= $urlImg; ?>)";>
+									
+										
+									<?php 
+										the_title(sprintf('<h1 class="entry-title"><a href="%s">', 
+													esc_url(get_permalink())), '</a></h1>');
+									?>
+
+									<small><?php the_category(' '); ?></small>
+								</div>
+							</div>
+							
+					<?php 
+								$i++; 
+							endwhile;
+						endif;
+					?>
+				</div>
 		</div>
 		<div class="col-xs-12 col-sm-4">
 			<?php get_sidebar(); ?>
--- page-home.php
+++ page-home.php
@@ -1,21 +1,132 @@
 <?php get_header(); ?>
 
 	<div class="row">
+
+
 	
-		<div class="col-xs-12">
+		<div class="row">
 			<?php  
-				$lastBlog = new 
-					WP_Query('type=post&posts_per_page=1');
 
-				if($lastBlog->have_posts()){
-					while($lastBlog->have_posts()){
-						$lastBlog->the_post();
-						get_template_part('content', get_post_format());
-					}
-				}
+				// $args_cat = array(
+				// 	'include' => '6,7,8',
+				// );
 
-				wp_reset_postdata();
+				// $categories = get_categories($args_cat);
+				// foreach($categories as $category):
+				// 	$args = [
+				// 		'type' => 'post',
+				// 		'posts_per_page' => 1,
+				// 		'category__in' => $category->term_id
+				// 	];
+
+				// 	$lastBlog = new WP_Query($args);
+
+				// 	if($lastBlog->have_posts()):
+				// 		while($lastBlog->have_posts()):
+				// 			$lastBlog->the_post();
+				// 			echo '<div class="col-xs-12 col-sm-4">';
+				// 				get_template_part('content', 'featured');
+				// 			echo '</div>';
+				// 		endwhile;
+				// 	endif;
+
+				// 	wp_reset_postdata();
+
+				// endforeach;
+
+				// $args = [
+				// 	'type' => 'post',
+				// 	'posts_per_page' => 3,
+				// 	'category__in' => array(6,7,8),
+				// 	'category__not_in' => array(9)
+				// ];
+
+				// $lastBlog = new 
+				// 	WP_Query($args);
+
+				// if($lastBlog->have_posts()){
+				// 	while($lastBlog->have_posts()){
+				// 		$lastBlog->the_post();
+				// 		echo '<div class="col-xs-12 col-sm-4">';
+				// 		get_template_part('content', 'featured');
+				// 		echo '</div>';
+				// 	}
+				// }
+
+				// wp_reset_postdata();
 			?>
+
+			<div class="col-xs-12">
+
+				<div id="awesome-carousel" class="carousel slide" data-ride="carousel">
+				  
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+					<?php 
+						$args_cat = array(
+						'include' => '6,7,8',
+						);
+
+						$categories = get_categories($args_cat);
+						$count = 0;
+						$bullets = '';
+						foreach($categories as $category):
+							$args = [
+								'type' => 'post',
+								'posts_per_page' => 1,
+								'category__in' => $category->term_id
+							];
+
+							$lastBlog = new WP_Query($args);
+							
+							if($lastBlog->have_posts()):
+								while($lastBlog->have_posts()):
+									$lastBlog->the_post(); 
+					?>
+
+									<div class="item <?php if($count==0) echo 'active'; ?>">
+								      <?php the_post_thumbnail('full'); ?>
+								      <div class="carousel-caption">
+								      	<?php the_title(sprintf('<h1 class="entry-title"><a href="%s">',
+								      		esc_url(get_permalink())),'</a></h1>'); ?>
+								      	<small><?php the_category(' '); ?></small>
+								      </div>
+								    </div>
+									<?php 
+										$bullets .= '<li data-target="#awesome-carousel" data-slide-to="'.$count.'" class="';
+										if($count == 0)
+											$bullets .= 'active';
+
+										$bullets .= '"></li>'; 
+									?>
+					<?php
+								endwhile;
+							endif;
+							$count++;
+							wp_reset_postdata();
+
+						endforeach;
+					?>
+
+					<!-- Indicators -->
+				  <ol class="carousel-indicators">
+				  	<?php echo $bullets; ?>
+				  </ol>
+
+				    
+				  </div>
+
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#awesome-carousel" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#awesome-carousel" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
+			</div>
 		</div>
 
 		<div class="col-xs-12 col-sm-8">
@@ -31,36 +142,36 @@
 
 				// print other 2 posts without the first one
 
-				$lastBlog = new
-					WP_Query('type=post&posts_per_page=2&offset=1');
-				if($lastBlog->have_posts()){
-					while($lastBlog->have_posts()){
-						$lastBlog->the_post();
-						get_template_part('content', get_post_format());
-					}
-				}
+				// $lastBlog = new
+				// 	WP_Query('type=post&posts_per_page=2&offset=1');
+				// if($lastBlog->have_posts()){
+				// 	while($lastBlog->have_posts()){
+				// 		$lastBlog->the_post();
+				// 		get_template_part('content', get_post_format());
+				// 	}
+				// }
 
-				wp_reset_postdata();
+				// wp_reset_postdata();
 			?>
 
-			<hr><hr><h2>tutorials</h2>
+			<!-- <hr><hr><h2>tutorials</h2> -->
 			<!-- print only tutorials -->
 			<?php 
-				$args = array(
-					'type' => 'post',
-					'posts_per_page' => -1,
-					'category_name' => 'tutorials',
-				);
-				$tutorials = new
-					WP_Query($args);
-				if($tutorials->have_posts()){
-					while($tutorials->have_posts()){
-						$tutorials->the_post();
-						get_template_part('content',get_post_format());
-					}
-				}
+				// $args = array(
+				// 	'type' => 'post',
+				// 	'posts_per_page' => -1,
+				// 	'category_name' => 'tutorials',
+				// );
+				// $tutorials = new
+				// 	WP_Query($args);
+				// if($tutorials->have_posts()){
+				// 	while($tutorials->have_posts()){
+				// 		$tutorials->the_post();
+				// 		get_template_part('content',get_post_format());
+				// 	}
+				// }
 
-				wp_reset_postdata();
+				// wp_reset_postdata();
 			?>
 		</div>
 		<div class="col-xs-12 col-sm-4">
